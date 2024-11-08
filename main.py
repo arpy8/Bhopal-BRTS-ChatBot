8eb155067160ff02a40b5a50f723f26eea33c005
Arpit <arpitsengar99@gmail.com>
2024-11-08 15:07:48 +0530

    fix: pysqlite3 version issue

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from st_pages.home_page import main as home_page
 from st_pages.overview_page import main as overview_page
