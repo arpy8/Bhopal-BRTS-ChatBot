c6f102de8dc99114a03cf2f2d58c670f125bd163
Arpit <arpitsengar99@gmail.com>
2024-11-24 03:31:26 +0530

    add contributors

--- st_pages/team_page.py
+++ st_pages/team_page.py
@@ -4,19 +4,24 @@ from utils.utils import update_page_state
 
 def main(team): 
     with team.container():
-        update_page_state("team")
-
+        st.balloons()
         st.write("""<br><center><h2>Meet the Team</h2></center><br>""", unsafe_allow_html=True)
 
-        a,b,c = st.columns([1,3,1])
+        a,_,b = st.columns([1.5,0.2,3])
+        
+        with open("assets/contributors.txt", "r") as f:
+            contributors = f.read().split('---')
+        
+        with a.container(border=True):
+            st.write(f"<br><br><br><center><h4>Chapter Lead</h4><p>Arpit Sengar</p></center>",unsafe_allow_html=True)
+            st.write(f"<center><h4>Chapter Co-Lead</h4><p>Rohit Dwivedi</p></center><br><br><br>",unsafe_allow_html=True)
         
         with b.container(border=True):
-            st.write("<br>" * 2, unsafe_allow_html=True)
+            st.write(f"<br><center><h4>Contributors</h4></center><br>",unsafe_allow_html=True)
             cols = st.columns([0.5,2,2,2,0.5])
-            for col in cols[1:4]:
+            
+            for idx, col in enumerate(cols[1:4]):
                 with col:
-                    st.write(
-                        f"<center><p>{'John Doe<br>'*10}</p></center>",
-                        unsafe_allow_html=True,
-                    )
+                    st.write(f"<center><p>{contributors[idx]}</p></center>",unsafe_allow_html=True)
+            
             st.write("<br>" * 2, unsafe_allow_html=True)
